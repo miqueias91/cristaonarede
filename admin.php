7e8465ae532ac24e9dc56097cad66fcd50aa9b03
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-02-17 19:36:17 -0300

    ok

---
+++ admin.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<?php 
+include_once "config/config.php";
+include_once "$CLASS_PATH/class.System.php";
+$sys = new System();
+
+?>
+<html lang="pt-br" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Diversas traduções e versões da Bíblia Sagrada, Quiz da Bíblia, Cifras e Harpa Cristã. Versículos e mensagens bíblicas da palavra de Deus diária para cada ocasião.">
+    <meta name="author" content="Innovate Soft, and Miqueias Matias Caetano">
+    <meta name="generator" content="Innovate Soft">
+    <title>Admin · Cristão na Rede</title>
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="./css/jquery-ui-1.12.1.custom/jquery-ui.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/fontawesome-free-5.6.3-web/css/all.css">
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" />
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="./css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center">
+    
+    <main class="form-signin">
+      <form>
+        <img class="mb-4" src="./img/logoresumidoazul.png" alt="" width="72" height="57">
+        <h1 class="h3 mb-3 fw-normal">ACESSO RESTRITO</h1>
+        <label for="inputEmail" class="visually-hidden">E-mail</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus>
+        <label for="inputPassword" class="visually-hidden">Senha</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
+        <br>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+      </form>
+    </main>
+
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/jquery-3.5.1.min.js"></script>  
+    <script src="./css/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+  </body>
+</html>
