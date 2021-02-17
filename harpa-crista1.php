72560424648f2399af859a41619465d7f4ee759d
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-02-17 12:20:01 -0300

    ok

---
+++ harpa-crista1.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<?php 
+include_once "config/config.php";
+
+?>
+<html lang="pt-br" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Diversas traduções e versões da Bíblia Sagrada, Quiz da Bíblia, Cifras e Harpa Cristã. Versículos e mensagens bíblicas da palavra de Deus diária para cada ocasião.">
+    <meta name="author" content="Innovate Soft, and Miqueias Matias Caetano">
+    <meta name="generator" content="Innovate Soft">
+    <title>Cristão na Rede · Conteúdo cristão de qualidade</title>
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="./css/jquery-ui-1.12.1.custom/jquery-ui.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <script data-ad-client="ca-pub-7091486462236476" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+  </head>
+  <body class="d-flex flex-column h-100">
+    <?php include_once './header.php'; ?>
+
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">
+      <section class="container">
+        <h1 class="mt-5">Harpa Cristã</h1>
+        <div class="col-md" id="listaharpa">
+          <br><br>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=1&titulo=CHUVAS DE GRAÇA">1 - CHUVAS DE GRAÇA</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=2&titulo=SAUDOSA LEMBRANÇA">2 - SAUDOSA LEMBRANÇA</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=3&titulo=PLENA PAZ">3 - PLENA PAZ</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=4&titulo=DEUS VELARÁ POR TI">4 - DEUS VELARÁ POR TI</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=5&titulo=Ó DESCE, FOGO SANTO">5 - Ó DESCE, FOGO SANTO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=6&titulo=NA MALDIÇÃO DA CRUZ">6 - NA MALDIÇÃO DA CRUZ</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=7&titulo=CRISTO CURA, SIM">7 - CRISTO CURA, SIM</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=8&titulo=CRISTO, O FIEL AMIGO">8 - CRISTO, O FIEL AMIGO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=9&titulo=MARCHAI, SOLDADOS DE CRISTO">9 - MARCHAI, SOLDADOS DE CRISTO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=10&titulo=EU TE LOUVO">10 - EU TE LOUVO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=11&titulo=Ó CRISTÃO, EIA AVANTE">11 - Ó CRISTÃO, EIA AVANTE</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=12&titulo=VEM JÁ, PECADOR">12 - VEM JÁ, PECADOR</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=13&titulo=JESUS COMPROU">13 - JESUS COMPROU</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=14&titulo=GOZO EM JESUS">14 - GOZO EM JESUS</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=15&titulo=CONVERSÃO">15 - CONVERSÃO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=16&titulo=DESPERTAR PARA O TRABALHO">16 - DESPERTAR PARA O TRABALHO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=17&titulo=PENSANDO EM JESUS">17 - PENSANDO EM JESUS</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=18&titulo=GRATA NOVA">18 - GRATA NOVA</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=19&titulo=O CONVITE DE CRISTO">19 - O CONVITE DE CRISTO</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=20&titulo=OLHAI PARA O CORDEIRO DE DEUS">20 - OLHAI PARA O CORDEIRO DE DEUS</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=21&titulo=GLORIOSA AURORA">21 - GLORIOSA AURORA</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=22&titulo=CEIA DO SENHOR">22 - CEIA DO SENHOR</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=23&titulo=GLÓRIA A JESUS">23 - GLÓRIA A JESUS</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=24&titulo=PODER PENTECOSTAL">24 - PODER PENTECOSTAL</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=25&titulo=JESUS, TU ÉS BOM">25 - JESUS, TU ÉS BOM</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=26&titulo=A FORMOSA JERUSALÉM">26 - A FORMOSA JERUSALÉM</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=27&titulo=AMOR QUE VENCE">27 - AMOR QUE VENCE</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=28&titulo=DEUS VAI TE GUIAR">28 - DEUS VAI TE GUIAR</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=29&titulo=O PRECIOSO SANGUE">29 - O PRECIOSO SANGUE</a></p>
+          <p><a class="conteudo-harpa lead" href="./texto-harpa.php?id=30&titulo=NÃO SOU MEU">30 - NÃO SOU MEU</a></p>
+        </div>
+      </section>
+    </main>
+    <!-- Anuncio1 -->
+    <ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-7091486462236476"
+    data-ad-slot="5148404228"
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <?php include_once './footer.php'; ?>
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/jquery-3.5.1.min.js"></script>  
+    <script src="./css/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+    <script type="text/javascript">
+      function listaHinario() {
+        var text = "";
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            $("#listaharpa").html('');
+            var data = JSON.parse(this.responseText);
+            data.forEach(function (hinos) {
+              text +='<p><a class="conteudo-harpa lead" href="./texto-harpa.php?id='+hinos['id']+'">'+hinos['id']+' - '+hinos['titulo']+'</a></p>'
+            });
+            $("#listaharpa").html('<br><br>'+text);
+          }
+        };
+        xmlhttp.open("GET", "files/harpa-crista/harpa.json", true);
+        xmlhttp.send();
+      }
+      $(function() {
+        //listaHinario();
+      });
+    </script>
+  </body>
+</html>
