94f2012daabd333d3bcc3e72fbdf555472680edd
Miqueias <miqueias.caetano@doctum.edu.br>
2021-02-25 10:54:00 -0300

    ok

--- header.php
+++ header.php
@@ -17,6 +17,34 @@
                 <a class="nav-link" aria-current="page" href="./">Início</a>
               </li>
               <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownStory" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Loja
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdownStory">
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=b4a22c34a9136fdaf41666ff22cb8e98&camp=1789&creative=9325&index=books&keywords=Bíblias">Bíblias</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/b?_encoding=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=c44abde5141a577b7d673475ea32d5ea&camp=1789&creative=9325&node=7874675011">Livros</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=84d230189a204705d2f30c6fc64b7ddd&camp=1789&creative=9325&index=books&keywords=catolicismo">Catolicismo</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=512b015aee30b1121e35104a56b228d9&camp=1789&creative=9325&index=books&keywords=teologia">Teologia</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=8117835ddda0f85bb6fa19c6b9703b18&camp=1789&creative=9325&index=books&keywords=vida cristã">Vida Cristã</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=fb65ae0ccd0f38f3d5674752e76c826c&camp=1789&creative=9325&index=books&keywords=judaísmo">Judaísmo</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=eb844acd3e92995861f8645e93ecd393&camp=1789&creative=9325&index=books&keywords=Estudos da Religião">Estudos da Religião</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Bíblia Sagrada
                 </a>
@@ -52,10 +80,8 @@
               </li>
               <li class="nav-item">
                 <a class="nav-link" target="_BLANK" href="https://bit.ly/3l9zjYi">Aplicativos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" target="_blank" href="https://www.amazon.com.br/b?_encoding=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=c44abde5141a577b7d673475ea32d5ea&camp=1789&creative=9325&node=7874675011">Livros Religiosos e Espirituais</a>
-              </li>
+              </li>           
+
 
               <li class="nav-item">
                 <a class="nav-link" target="_BLANK" href="https://www.instagram.com/cristao.narede/">Contato</a>
