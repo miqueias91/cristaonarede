88d1d468d3eef3c6b92681b31390615d129fda83
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-02-27 18:09:32 -0300

    ok

--- header.php
+++ header.php
@@ -82,10 +82,14 @@
                 <a class="nav-link" target="_BLANK" href="https://bit.ly/3l9zjYi">Aplicativos</a>
               </li>           
 
-
               <li class="nav-item">
                 <a class="nav-link" target="_BLANK" href="https://www.instagram.com/cristao.narede/">Contato</a>
               </li>
+	           	<li class="nav-item">
+	                <a class="nav-link" target="_BLANK" href="https://www.paypal.com/donate?hosted_button_id=UVT2DU89C2X6N">DOE AGORA</a>
+	            </li>
+
+
             </ul>
           </div>
         </div>
