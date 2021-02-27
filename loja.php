0ba5cc34344b5ef81a64a696dc64e5704a389271
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-02-27 11:36:47 -0300

    ok

---
+++ loja.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<?php 
+include_once "config/config.php";
+include_once "$CLASS_PATH/class.Biblia.php";
+include_once "$CLASS_PATH/class.System.php";
+$sys = new System();
+if (!$ambiente_desenvolvimento) {
+  $sys->registraAcesso();
+}
+
+?>
+<html lang="pt-br" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Diversas traduções e versões da Bíblia Sagrada, Quiz da Bíblia, Cifras e Harpa Cristã. Versículos e mensagens bíblicas da palavra de Deus diária para cada ocasião.">
+    <meta name="author" content="Innovate Soft, and Miqueias Matias Caetano">
+    <meta name="generator" content="Innovate Soft">
+    <title>Cristão na Rede · Conteúdo cristão de qualidade</title>
+    
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/sticky-footer-navbar.css" rel="stylesheet">
+    <link href="./css/jquery-ui-1.12.1.custom/jquery-ui.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./css/fontawesome-free-5.6.3-web/css/all.css">
+    <link href="./css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/favicon.ico" />
+    <script data-ad-client="ca-pub-7091486462236476" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+      .card {
+        margin-top: 10px;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+  </head>
+  <body class="d-flex flex-column h-100">
+    <?php include_once './header.php'; ?>
+
+    <!-- Begin page content -->
+    <main class="flex-shrink-0">  
+
+
+      <section class="container">
+        <h1 class="mt-5">Loja</h1>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=b4a22c34a9136fdaf41666ff22cb8e98&camp=1789&creative=9325&index=books&keywords=Bíblias">Bíblias
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/b?_encoding=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=c44abde5141a577b7d673475ea32d5ea&camp=1789&creative=9325&node=7874675011">Livros
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=84d230189a204705d2f30c6fc64b7ddd&camp=1789&creative=9325&index=books&keywords=catolicismo">Catolicismo
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+          </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=512b015aee30b1121e35104a56b228d9&camp=1789&creative=9325&index=books&keywords=teologia">Teologia
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=8117835ddda0f85bb6fa19c6b9703b18&camp=1789&creative=9325&index=books&keywords=vida cristã">Vida Cristã
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=fb65ae0ccd0f38f3d5674752e76c826c&camp=1789&creative=9325&index=books&keywords=judaísmo">Judaísmo
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+          <div class="row card">
+            <div class="col-md-12 card-body">
+              <a class="dropdown-item" target="_blank" href="https://www.amazon.com.br/gp/search?ie=UTF8&tag=miqueias91-20&linkCode=ur2&linkId=eb844acd3e92995861f8645e93ecd393&camp=1789&creative=9325&index=books&keywords=Estudos da Religião">Estudos da Religião
+              <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
+            </div>
+          </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      </section>
+    </main>
+
+    <!-- Anuncio1 -->
+    <ins class="adsbygoogle"
+    style="display:block"
+    data-ad-client="ca-pub-7091486462236476"
+    data-ad-slot="5148404228"
+    data-ad-format="auto"
+    data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    
+    <?php include_once './footer.php'; ?>
+
+    <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/jquery-3.5.1.min.js"></script>  
+    <script src="./css/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script>
+
+    <script src="https://www.gstatic.com/firebasejs/8.2.8/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.2.8/firebase-analytics.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.2.8/firebase-database.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/8.2.8/firebase-auth.js"></script>
+    <script>
+      // Your web app's Firebase configuration
+      // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+      var firebaseConfig = {
+        apiKey: "AIzaSyBcEM9D2fDvZKd1GRR6dB8S_tAV72ZfVXc",
+        authDomain: "cristao-na-rede.firebaseapp.com",
+        projectId: "cristao-na-rede",
+        storageBucket: "cristao-na-rede.appspot.com",
+        messagingSenderId: "356851272550",
+        appId: "1:356851272550:web:d6f0571eed0c2eacccd6a7",
+        measurementId: "G-FNSSN115D1"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig);
+      firebase.analytics();
+
+
+      firebase.auth().signInAnonymously().catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage)
+      });
+    </script>
+
+  </body>
+</html>
