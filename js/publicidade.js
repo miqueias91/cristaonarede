73b841abcd0f8638b531fc2948579051927543a4
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-04 10:56:04 -0300

    ok

--- js/publicidade.js
+++ js/publicidade.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+  var array_publicidade_img = new Array();
+  var array_publicidade_texto = new Array();
+  var array_publicidade_link = new Array();
+
+  array_publicidade_img = ['publicidade_0.png'];
+  array_publicidade_texto = ['Estude a Bíblia de uma forma Sistemática com 40 Disciplinas, em uma Plataforma online preparada para você com Vídeo Aulas, Apostilas em PDF Fóruns um Grupo Fechado, Certificado Online e Histórico Escolar, com um Professor com mais de 20 anos de Experiência Teológica.'];
+  array_publicidade_link = ['https://go.hotmart.com/W48847673Y'];
+
+  var img = array_publicidade_img[0];
+  var texto = array_publicidade_texto[0];
+  var link = array_publicidade_link[0];
+
   var html_publicidade = 
       '<div class="modal" id="myModal" tabindex="-1" role="dialog">'+
       '  <div class="modal-dialog" role="document">'+
@@ -10,11 +22,11 @@ $(document).ready(function(){
       '        </button>'+
       '      </div>'+
       '      <div class="modal-body">'+
-      '        <img src="./files/publicidades/publicidade_0.png" alt="..." class="img-thumbnail">'+
-      '        <p>Estude a Bíblia de uma forma Sistemática com 40 Disciplinas, em uma Plataforma online preparada para você com Vídeo Aulas, Apostilas em PDF Fóruns um Grupo Fechado, Certificado Online e Histórico Escolar, com um Professor com mais de 20 anos de Experiência Teológica.</p>'+
+      '        <img src="./files/publicidades/'+img+'" alt="..." class="img-thumbnail">'+
+      '        <p>'+texto+'</p>'+
       '      </div>'+
       '      <div class="modal-footer">'+
-      '        <a href="https://go.hotmart.com/W48847673Y" target="T_BLANK" class="btn btn-secondary acessar" data-dismiss="modal">Saber mais</a>'+
+      '        <a href="'+link+'" target="T_BLANK" class="btn btn-secondary acessar" data-dismiss="modal">Saber mais</a>'+
       '      </div>'+
       '    </div>'+
       '  </div>'+
