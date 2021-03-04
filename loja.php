3a27b7f1f52c1c36ee54ba0fd82d8f7ae723b643
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-04 10:12:52 -0300

    ok

--- loja.php
+++ loja.php
@@ -101,23 +101,27 @@ if (!$ambiente_desenvolvimento) {
               <span style="float:right"><i class="fas fa-external-link-alt"></i></span></a>
             </div>
           </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
       </section>
+
+
+<div class="modal" id="myModal" tabindex="-1" role="dialog">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Publicidade</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Estude a Bíblia de uma forma Sistemática com 40 Disciplinas, em uma Plataforma online preparada para você com Vídeo Aulas, Apostilas em PDF Fóruns um Grupo Fechado, Certificado Online e Histórico Escolar, com um Professor com mais de 20 anos de Experiência Teológica.</p>
+      </div>
+      <div class="modal-footer">
+        <a href="https://go.hotmart.com/W48847673Y" target="T_BLANK" class="btn btn-secondary acessar" data-dismiss="modal">Saber mais</a>
+      </div>
+    </div>
+  </div>
+</div>
     </main>
 
     <!-- Anuncio1 -->
@@ -144,6 +148,10 @@ if (!$ambiente_desenvolvimento) {
     <script>
       $(function() {
 
+        $('#myModal').modal('show');
+        $( ".close" ).click(function() {
+          $('#myModal').modal('hide');
+        }); 
 
       });
     </script>
