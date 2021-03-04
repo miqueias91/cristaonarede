07cb3a20f374f206be4e394d2356b529f8bdcc46
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-04 10:23:12 -0300

    ok

--- loja.php
+++ loja.php
@@ -104,24 +104,25 @@ if (!$ambiente_desenvolvimento) {
       </section>
 
 
-<div class="modal" id="myModal" tabindex="-1" role="dialog">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Publicidade</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+      <div class="modal" id="myModal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Publicidade</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <img src="./files/publicidades/publicidade_0.png" alt="..." class="img-thumbnail">
+              <p>Estude a Bíblia de uma forma Sistemática com 40 Disciplinas, em uma Plataforma online preparada para você com Vídeo Aulas, Apostilas em PDF Fóruns um Grupo Fechado, Certificado Online e Histórico Escolar, com um Professor com mais de 20 anos de Experiência Teológica.</p>
+            </div>
+            <div class="modal-footer">
+              <a href="https://go.hotmart.com/W48847673Y" target="T_BLANK" class="btn btn-secondary acessar" data-dismiss="modal">Saber mais</a>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="modal-body">
-        <p>Estude a Bíblia de uma forma Sistemática com 40 Disciplinas, em uma Plataforma online preparada para você com Vídeo Aulas, Apostilas em PDF Fóruns um Grupo Fechado, Certificado Online e Histórico Escolar, com um Professor com mais de 20 anos de Experiência Teológica.</p>
-      </div>
-      <div class="modal-footer">
-        <a href="https://go.hotmart.com/W48847673Y" target="T_BLANK" class="btn btn-secondary acessar" data-dismiss="modal">Saber mais</a>
-      </div>
-    </div>
-  </div>
-</div>
     </main>
 
     <!-- Anuncio1 -->
