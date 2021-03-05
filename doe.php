e9a4e3d6d3b4b6489aca5736c21a011b988ea2ce
Miqueias Aspire-E1-571 <miqueiasmcaetano@gmail.com>
2021-03-04 23:38:12 -0300

    ok

--- doe.php
+++ doe.php
@@ -1,2 +1,9 @@
-<?php
+<?php 
+include_once "config/config.php";
+include_once "$CLASS_PATH/class.Biblia.php";
+include_once "$CLASS_PATH/class.System.php";
+$sys = new System();
+if (!$ambiente_desenvolvimento) {
+  $sys->registraAcesso();
+}
 header('Location: https://www.paypal.com/donate?hosted_button_id=UVT2DU89C2X6N');
