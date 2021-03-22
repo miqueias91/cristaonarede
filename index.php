411ed33b99ed22ec6b2752de7063fc2746c5ac09
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-22 09:05:21 -0300

    ok

--- index.php
+++ index.php
@@ -136,17 +136,6 @@ $mensagem = $mensagem[0];
 
     <!-- Begin page content -->
     <main class="flex-shrink-0">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-      <!-- Vertical 1 -->
-      <ins class="adsbygoogle"
-           style="display:block"
-           data-ad-client="ca-pub-7091486462236476"
-           data-ad-slot="8888783157"
-           data-ad-format="auto"
-           data-full-width-responsive="true"></ins>
-      <script>
-           (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
 
       <section class="container">
         <h1 class="mt-5">Recomendados para você</h1>
