cdf914776961763bcae4b17788b916e7c3e64a95
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-29 15:41:32 -0300

    ok

--- aplicativos.php
+++ aplicativos.php
@@ -1,11 +1,9 @@
-<?php
-
+<?php 
 include_once "config/config.php";
-include_once "$CLASS_PATH/class.system.php";
-
+include_once "$CLASS_PATH/class.Biblia.php";
+include_once "$CLASS_PATH/class.System.php";
 $sys = new System();
 if (!$ambiente_desenvolvimento) {
   $sys->registraAcesso();
 }
-
 echo "<script>window.location.href='https://play.google.com/store/apps/developer?id=Innovate+Soft';</script>";
