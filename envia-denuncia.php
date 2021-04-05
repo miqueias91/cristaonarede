28e172df0e1bf6bbce5d4fc934a3b86feaf941c6
Miqueias <miqueias.caetano@doctum.edu.br>
2021-04-05 18:12:03 -0300

    ok

--- envia-denuncia.php
+++ envia-denuncia.php
@@ -5,7 +5,8 @@ include_once "$CLASS_PATH/class.Dicionario.php";
 
 $sys = new System();
 $dc = new Dicionario();
-
+echo "<pre>";print_r($_GET);
+echo "<pre>";print_r($_POST);die;
 if ($_POST) {
 	$dc->registraDenunciaDicionario($_GET['id'], $_POST['userId'], $_POST['motivo']);
 	echo "<script>alert('Denúncia enviada com sucesso.');window.location.href='./texto-dicionario.php?id=$_GET[id]';</script>";
